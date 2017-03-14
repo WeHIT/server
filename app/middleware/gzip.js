8c739f19f48012486d5af6bbc808eaf88d366fba
rccoder <rccoder.net@gmail.com>
2017-03-14 16:27:26 +0800

    feat: add gzip middleware and replace npm@4 to yarn

---
+++ app/middleware/gzip.js
@@ -0,0 +1,23 @@
+/**
+ * Created by rccoder on 03/14/2017.
+ */
+const isJSON = require('koa-is-json');
+const zlib = require('zlib');
+
+module.exports = (options, app) => {
+  return function* gzipMiddleware(next) {
+    yield next;
+    const body = this.body;
+    if(!body){
+      return;
+    }
+    if(options.threshold && this.length < options.threshold) {
+      return;
+    }
+    if(isJSON(body)) {
+      body = JSON.stringify(body);
+      this.body = zlib.createGzip().end(body);
+      this.set('Content-Encoding', 'gzip');
+    }
+  }
+}
--- config/config.default.js
+++ config/config.default.js
@@ -1,22 +1,22 @@
-'use strict',
+'use strict';
 
-module.exports  = appInfo  => {
+module.exports = appInfo => {
 
   return {
     // should change to your own
-    keys  : appInfo.name + '123456',
+    keys: appInfo.name + '123456',
 
     // 今日哈工大地址
-    todayUrl  : 'http://today.hit.edu.cn',
+    todayUrl: 'http://today.hit.edu.cn',
     // 今日哈工大返回 pageSize
-    todaySize  : 3,
+    todaySize: 3,
     // 今日哈工大爬虫地址 MAP
-    todayMap  : {
+    todayMap: {
       // 管理学院
-      'MANAGEMENT': 3
+      MANAGEMENT: 3,
     },
     // 今日哈工大特定新闻打底缩略图
-    todayNewsFirstImageDefaultArr  : [
+    todayNewsFirstImageDefaultArr: [
       'https://okujk9avg.bkt.clouddn.com/20170302148846166491367.png',
       'https://okujk9avg.bkt.clouddn.com/20170302148846173968927.png',
       'https://okujk9avg.bkt.clouddn.com/20170302148846178813468.png'
@@ -24,20 +24,24 @@ module.exports  = appInfo  => {
 
     // 心知天气 API 需要
     // https://api.thinkpage.cn/v3/weather/now.json?key :vzjfy7yvepyzv0ay&location :haerbin&language :zh-Hans&unit :c
-    weatherUrl  : 'https://api.thinkpage.cn/v3/weather/now.json',
-    weatherKey  : 'vzjfy7yvepyzv0ay',
-    weatherErrorTips  : '亲！天气查询接口好像疯了~',
+    weatherUrl: 'https://api.thinkpage.cn/v3/weather/now.json',
+    weatherKey: 'vzjfy7yvepyzv0ay',
+    weatherErrorTips: '亲！天气查询接口好像疯了~',
 
     // middleware
-    middleware  : [
-      'robot'
+    middleware: [
+      'robot',
+      'gzip',
     ],
 
     // detail for middleware
-    robot  : {
+    robot: {
       ua: [
         /Baiduspider/i,
-      ]
+      ],
     },
+    gzip: {
+      threshold: 1024
+    }
   };
-}
+};
