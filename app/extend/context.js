260ebafa68666561bc88b438e10d139b84ac7fb2
rccoder <rccoder.net@gmail.com>
2017-03-18 17:07:24 +0800

    feat: test schedule and add extends

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,9 +1,11 @@
 /**
  * Created by rccoder on 28/02/2017.
  */
+'use strict';
+
 module.exports = {
   get isIOS() {
-      const iosReg = /iphone|ipad|ipod/i;
-      return iosReg.test(this.get('user-agent'));
-  }
-}
+    const iosReg = /iphone|ipad|ipod/i;
+    return iosReg.test(this.get('user-agent'));
+  },
+};
--- app/extend/helper.js
+++ app/extend/helper.js
@@ -2,6 +2,8 @@
  * Created by rccoder on 02/03/2017.
  */
 
+'use strict';
+
 const moment = require('moment');
 
 exports.relativeTime = time => moment(new Date(time * 1000)).fromNow();
