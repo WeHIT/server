b75a6b427de2a5b035759c63119dd4f57c10a684
rccoder <rccoder.net@gmail.com>
2017-04-04 23:39:47 +0800

    feat: express content splice

--- app/controller/api.js
+++ app/controller/api.js
@@ -31,9 +31,22 @@ module.exports = app => {
       switch (command) {
         case 'common': {
           const { data } = options;
-          if (data === '查快递' || data === '快递') {
-            const data = yield this.service.express.getHistoryInfo(this.ctx.state.user.username);
-            this.ctx.body = data;
+          if (data === '查快递' || data === '快递' || data === '查询快递') {
+            const result = yield this.service.express.getHistoryInfo(this.ctx.state.user.username);
+            console.log(JSON.stringify(result));
+            let data = `<p>你的历史快递查询中共有${result.length}个快递没有签收<p>`;
+            for (let i = 0; i < result.length; i++) {
+              data += `<p>单号${result[i].logisticCode}：</p>`;
+              for (let j = 0; j < result[i].content.length; j++) {
+                data += `<p>${result[i].content[j].shipperName}</p>`;
+                for (let k = 0; k < result[i].content[j].Traces.length; k++) {
+                  data += `<p>${result[i].content[j].Traces[i].AcceptTime}</p><p>${result[i].content[j].Traces[i].AcceptStation}</p>`;
+                }
+              }
+            }
+            this.success({
+              data,
+            });
           }
           return;
         }
